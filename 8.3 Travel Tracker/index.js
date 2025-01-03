51be9b7f7fcb1b61fe1fe0e3dc7042656072a2c1
Ericson Castasus <castasuse@gmail.com>
2025-01-03 11:30:26 +0800

    add new route

--- 8.3 Travel Tracker/index.js
+++ 8.3 Travel Tracker/index.js
@@ -21,10 +21,27 @@ db.connect();
 app.get("/", async (req, res) => {
   const result = await db.query("SELECT country_code FROM visited_countries");
   const countries = result.rows.map((item) => {
-    return item.country_code
+    return item.country_code;
   });
 
   res.render("index.ejs", { countries: countries, total: result.rowCount });
+});
+
+app.post("/add", async (req, res) => {
+  const country = req.body.country;
+  const result = await db.query(
+    "SELECT country_code FROM world_countries WHERE country_name=$1",
+    [country]
+  );
+  if (result.rowCount > 0) {
+    const countryCode = result.rows[0].country_code;
+
+    const insert = await db.query(
+      "INSERT INTO visited_countries (country_code) VALUES ($1)",
+      [countryCode]
+    );
+    res.redirect("/");
+  }
 });
 
 app.listen(port, () => {
